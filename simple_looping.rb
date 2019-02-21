f207c40476e4c33e58e435b703f163fbae62de21
fortunate-partial-4781 <niko876@hotmail.com>
2019-02-21 03:06:09 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -35,8 +35,8 @@ end
 
 def for_iterator(number_of_times)
   counter = 1..number_of_times
-  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  for count in counter
+    puts = "Welcome to Flatiron School's Web Development Course!"
+  end
 end
 
