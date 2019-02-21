7f7119614c1b69c00d781d0b74f5e1b1109ed3d1
fortunate-partial-4781 <niko876@hotmail.com>
2019-02-21 01:55:16 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,7 +4,7 @@ def loop_iterator(number_of_times)
   counter = 0
   loop do
     puts "Welcome to Flatiron School's Web Development Course!"
-    break if counter >= number_of_times
+    if counter >= number_of_times
     counter = counter + 1
   end
 end
