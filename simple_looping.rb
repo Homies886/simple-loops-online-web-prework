0135e0988ea16bb1b3ba2ffd8c5b64515ebdcb97
fortunate-partial-4781 <niko876@hotmail.com>
2019-02-21 02:50:50 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -27,7 +27,7 @@ end
 
 def until_iterator(number_of_times)
   counter = 0
-  until counter == number_of_times
+  until counter === number_of_times
     counter += 1
     puts = "Welcome to Flatiron School's Web Development Course!"
   end
